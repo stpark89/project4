e1c1112e3814c22edd4686773a96ff40f08dc02c
하재현 <gifmgo@naver.com>
2016-12-30 17:49:50 +0900

    최신뉴스

--- StartUp/src/main/webapp/dashboard/News.js
+++ StartUp/src/main/webapp/dashboard/News.js
@@ -10,22 +10,6 @@ $(function(){
 		success : function(data){
 			$('#mainNews').empty();
 			$('#goUl').empty();
-			//console.log(data.Goyoung);
-			$.each(data.Goyoung.channel,function(index, value){
-				//var date = new Date(value['date']);
-				//var m = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate();
-				if(index == 'item'){
-				
-					var test = value[0];
-					//console.log(test["dc:date"]["#text"]+'2222');
-					////씨발     test 찍어봡
-					var date = new Date(test["date"]);
-					//console.log(date);
-					var m = date.getFullYear() + "-" +(date.getMonth())+"-"+date.getDate();
-					//console.log(m);
-				}
-				 
-			});
 			
 			$.each(data.Goyoung.channel.item,function(index, value){
 
@@ -36,22 +20,43 @@ $(function(){
 					var link = value.link;
 					var date = value["dc:date"]["#text"];
 					$('#mainNews').html("<a href="+value.link+">"+title+"</a>"+date);
-				}else if(index >= 1 && index <= 4){
-					console.log("엘스 이프 : "+index);
+				}else if(index >= 1 && index < 4){
 					var title = value.title;
 					var link = value.link;
 					var date = value["dc:date"]["#text"];
 					$('#goUl').append("<li><a href="+value.link+">"+title+"</a><span>"+date+"</span></li>");
 				}
 				//제목 - 링크 뽑음 - console.log(value.title + " / link : "+value.link);
-				console.log(value.title + " / link : "+value.link+"/ "+value["dc:date"]["#text"]);
+				//console.log(value.title + " / link : "+value.link+"/ "+value["dc:date"]["#text"]);
 				
 			});
-			
-			//var length = data.Goyoung.item.length;
-
 		
 		}
 	});
 	
+	$.ajax({
+		
+	    url:"CommunityNews_2.do",
+		dataType:"json",
+		success:function(data){
+			$('#mainNews2').empty();
+			$.each(data.joinsmsn.channel.item, function(index,obj){
+				//console.log(obj.title+"////"+obj.link+"////"+obj.pubDate);
+				var title = obj.title;
+				var link = obj.link;
+				var date = obj.pubDate;
+				console.log(date);
+				if(index==0){
+					$('#mainNews2').html("<a href="+link+">"+title+"</a>"+date);
+				}else if(index>=1 && index<4){
+					$('#goUl2').append("<li><a href="+link+">"+title+"</a><span>"+date+"</span></li>");
+				}
+			});
+		}
+		
+	});
+	
+	
 });
+
+
