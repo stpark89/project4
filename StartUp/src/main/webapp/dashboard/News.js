996f42e56545645c1cc67a934252e3cee8d48a34
하재현 <gifmgo@naver.com>
2016-12-30 15:38:18 +0900

    commit

--- StartUp/src/main/webapp/dashboard/News.js
+++ StartUp/src/main/webapp/dashboard/News.js
@@ -8,23 +8,6 @@ $(function(){
 		url : "CommunityNews_1.do",
 		dataType : "json",
 		success : function(data){
-			//console.log(data.Goyoung);
-			$.each(data.Goyoung.channel,function(index, value){
-				//var date = new Date(value['date']);
-				//var m = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate();
-				if(index == 'item'){
-				
-					var test = value[0];
-					//console.log(test["dc:date"]["#text"]+'2222');
-					////씨발     test 찍어봡
-					var date = new Date(test["date"]);
-					//console.log(date);
-					var m = date.getFullYear() + "-" +(date.getMonth())+"-"+date.getDate();
-					//console.log(m);
-				}
-				 
-			});
-			
 			$.each(data.Goyoung.channel.item,function(index, value){
 					
 				//한줄 리스트 있는것
